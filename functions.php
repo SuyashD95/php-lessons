3003587d537245dda606f3c9cf7de726bca81879
Suyash Dayal <15222897+SuyashD95@users.noreply.github.com>
2018-06-30 23:28:56 +0530

    Completed a lesson on functions
    Learnt about different types of functions including using default values
    and passing arguments to the functions either by value or by reference

---
+++ functions.php
@@ -0,0 +1,51 @@
+<?php
+	
+	# FUNCTION - Block of code that can be repeatedly called
+	
+	/*
+		Camel Case - myFunction()
+		Lower Case - my_function()
+		Pascal Case - MyFunction()
+	 */
+	
+	// Create Simple Function
+	function simpleFunction() {
+		echo "Hello World";
+	}
+
+	// Run Simple Function
+	// simpleFunction();
+
+	// Function with Params and Default Values
+	function sayHello($name = 'World') {
+		echo "Hello $name<br>";
+	}	
+
+	// sayHello('Joe');
+	// sayHello('Bob');
+	// sayHello();
+	
+	// Return Value
+	function addNumbers($num1, $num2) {
+		return $num1 + $num2;
+	}
+
+	// echo addNumbers(2, 3);
+
+	// By Reference
+	$myNum = 10;
+
+	function addFive($num) { 
+		$num += 5;
+	}
+
+	function addTen(&$num) {
+		$num += 10;
+	}
+
+	addFive($myNum);
+	echo "Value: $myNum<br>";
+
+	addTen($myNum);
+	echo "Value: $myNum<br>";
+?>
